54432dfee38d65e21e98a6fb4068c0c448b6ae34
MagriLuca <142036718+MagriLuca@users.noreply.github.com>
2023-08-12 20:43:13 +0200

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -1,4 +1,7 @@
 # Questo è il primo script che è stato adoperato a lezione
 
+# install.packages("raster")
 library(raster)
 
+#Comando per selezionare la cartella di lavoro in Windows
+setwd("C:/lab/")
