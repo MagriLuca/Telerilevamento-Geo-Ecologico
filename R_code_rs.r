806f338d155f6c44bd049faace0650c17ae13720
MagriLuca <142036718+MagriLuca@users.noreply.github.com>
2023-08-13 18:42:56 +0200

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -5,3 +5,19 @@ library(raster)
 
 #Comando per selezionare la cartella di lavoro in Windows
 setwd("C:/lab/")
+
+# Importo immagine da cartella personale
+l2011 <- brick("p224r63_2011.grd")
+
+# Richiamo l'oggetto ed osservo le proprietà
+l2011
+
+# Plot del file prima richiamato
+plot(l2011)
+
+# Modifico i colori della legenda per accessibilità
+cl <- colorRampPalette(c("black","grey","light grey")) (100)
+
+# Plot nuovo con legenda aggiornata da nero a bianco
+plot(l2011, col=cl)
+
