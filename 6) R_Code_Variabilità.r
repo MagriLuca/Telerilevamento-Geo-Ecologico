68a565270dd8c87ac2001d091a33340c13fee916
MagriLuca <142036718+MagriLuca@users.noreply.github.com>
2023-08-26 14:08:42 +0200

    Create 6) R_Code_Variabilità.r

---
+++ 6) R_Code_Variabilità.r
@@ -0,0 +1,73 @@
+# Script per il calcolo della variabilità
+
+library(raster)
+library(RStoolbox)
+library(ggplot2)
+library(patchwork)
+library(viridis)
+
+setwd("C:/lab/")
+
+# Caricamento dell'immagine del Similaun
+sen <- brick("sentinel.png")
+
+# Banda 1 = NIR
+# Banda 2 = Rosso
+# Banda 3 = Verde
+
+# Plot dell'immagine usando funzione ggRGB
+plotRGB(sen, 1, 2, 3, stretch="lin")
+
+# NIR impostato sulla componente del verde
+plotRGB(sen, 2, 1, 3)
+
+# Calcolo della variabilità adoperando il NIR
+nir <- sen[[1]]
+
+sd3 <- focal(nir, matrix(1/9, 3, 3), fun=sd)
+
+clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) #
+plot(sd3, col=clsd)
+
+# Plot adoperando ggplot
+sd3d <- as.data.frame(sd3, xy=T)
+
+ggplot() +
+geom_raster(sd3d, mapping=aes(x=x, y=y, fill=layer))
+
+# Cromatura Viridis
+ggplot() +
+geom_raster(sd3d, mapping =aes(x=x, y=y, fill=layer)) +
+scale_fill_viridis() +
+ggtitle("Standard deviation by viridis package")
+
+# Cromatura Cividis
+ggplot() +
+geom_raster(sd3d, mapping =aes(x=x, y=y, fill=layer)) +
+scale_fill_viridis(option = "cividis") +
+ggtitle("Standard deviation by viridis package")
+
+# Cromatura Magma
+ggplot() +
+geom_raster(sd3d, mapping =aes(x=x, y=y, fill=layer)) +
+scale_fill_viridis(option = "magma") +
+ggtitle("Standard deviation by viridis package")
+
+# Cromatura Inferno
+ggplot() +
+geom_raster(sd3d, mapping =aes(x=x, y=y, fill=layer)) +
+scale_fill_viridis(option = "inferno") +
+ggtitle("Standard deviation by viridis package")
+
+# Adoperando il pacchetto patchwork
+p1 <- ggplot() +
+geom_raster(sd3d, mapping =aes(x=x, y=y, fill=layer)) +
+scale_fill_viridis() +
+ggtitle("Standard deviation by viridis package")
+
+p2 <- ggplot() +
+geom_raster(sd3d, mapping =aes(x=x, y=y, fill=layer)) +
+scale_fill_viridis(option = "inferno") +
+ggtitle("Standard deviation by viridis package")
+
+p1 + p2
