4a4b0cd170f6bb947f8689ae6d9c3d00026a0896
MagriLuca <142036718+MagriLuca@users.noreply.github.com>
2023-08-26 15:07:18 +0200

    Update 9) R_Code_Analisi_Con_LIDAR.r

--- 9) R_Code_Analisi_Con_LIDAR.r
+++ 9) R_Code_Analisi_Con_LIDAR.r
@@ -1,34 +1,28 @@
-#
+# Script per lavorare con dati LIDAR
 
-library(raster) #"Geographic Data Analysis and Modeling"
-library(rgdal) #"Geospatial Data Abstraction Library"
+library(raster)
+library(rgdal)
 library(viridis)
 library(ggplot2)
 
-# set working directory
-setwd("C:/Users/MiTorresani/OneDrive - Alma Mater Studiorum Universit� di Bologna/lezioni/UniBo/esercizi/risultati")
+setwd("C:/lab/")
 
-# load dsm 2013
-dsm_2013 <- raster("C:/Users/MiTorresani/OneDrive - Alma Mater Studiorum Universit� di Bologna/lezioni/UniBo/esercizi/dati/2013Elevation_DigitalElevationModel-0.5m.tif")
-
-# view info about the raster.
+# Caricare il file DSM 2013
+dsm_2013 <- raster(2013Elevation_DigitalElevationModel-0.5m.tif")
 dsm_2013
 
-# plot the DSM 2013
+# Plot del file DSM 2013
 plot(dsm_2013, main="Lidar Digital Surface Model San Genesio/Jenesien")
 
-# load dtm 2013
-dtm_2013 <- raster("C:/Users/MiTorresani/OneDrive - Alma Mater Studiorum Universit� di Bologna/lezioni/UniBo/esercizi/dati/2013Elevation_DigitalTerrainModel-0.5m.tif")
+# Caricare il file DTM 2013
+dtm_2013 <- raster(2013Elevation_DigitalTerrainModel-0.5m.tif")
 
-#plot dtm 2013
+# Plot del file DTM 2013
 plot(dtm_2013, main="Lidar Digital Terrain Model San Genesio/Jenesien")
 
-# create CHM 2013 as difference between dsm and dtm
+# Creazione del CHM 2013 come differenza tra DSM e DTM
 chm_2013 <- dsm_2013 - dtm_2013
-
-# view CHM attributes
 chm_2013
-
 
 chm_2013d <- as.data.frame(chm_2013, xy=T)
 
@@ -37,74 +31,60 @@ ggplot() +
   scale_fill_viridis() +
   ggtitle("CHM 2013 San Genesio/Jenesien")
 
-#save the CHM on computer
+# Salvataggio del file CHM sul computer
 writeRaster(chm_2013,"chm_2013_San_genesio.tif")
 
-
-# load dsm 2004
-dsm_2004 <- raster("C:/Users/MiTorresani/OneDrive - Alma Mater Studiorum Universit� di Bologna/lezioni/UniBo/esercizi/dati/2004Elevation_DigitalElevationModel-2.5m.tif")
-
-# view info about the raster.
+# Caricare il file DSM 2004
+dsm_2004 <- raster(2004Elevation_DigitalElevationModel-2.5m.tif")
 dsm_2004
 
-# plot the DSM 2004
+# Plot del file DSM 2004
 plot(dsm_2004, main="Lidar Digital Surface Model San Genesio/Jenesien")
 
-# load dtm 2004
-dtm_2004 <- raster("C:/Users/MiTorresani/OneDrive - Alma Mater Studiorum Universit� di Bologna/lezioni/UniBo/esercizi/dati/2004Elevation_DigitalTerrainModel-2.5m.tif")
+# Caricare il file DTM 2004
+dtm_2004 <- raster(2004Elevation_DigitalTerrainModel-2.5m.tif")
 
-#plot dtm 2004
+# Plot del file DTM 2004
 plot(dtm_2004, main="Lidar Digital Terrain Model San Genesio/Jenesien")
 
-# create CHM 2004 as difference between DSM and DTM
+# Creazione del CHM 2004 come differenza tra DSM e DTM
 chm_2004 <- dsm_2004 - dtm_2004
-
-# view CHM attributes
 chm_2004
 
 chm_2004d <- as.data.frame(chm_2004, xy=T)
 
-#plot CHM 2004
 ggplot() +
   geom_raster(chm_2004d, mapping =aes(x=x, y=y, fill=layer)) +
   scale_fill_viridis() +
   ggtitle("CHM 2004 San Genesio/Jenesien")
 
-
-#save CHM on computer
+# Salvataggio del file CHM sul computer
 writeRaster(chm_2004,"chm_2004_San_genesio.tif")
 
+# Nota Importante: scritto così è errore di risoluzione
+difference_chm <- chm_2013 - chm_2004
 
-#error
-difference_chm<-chm_2013-chm_2004
+# Funzione di Reseample portando il 2013 al 2004 (2.5m)
+chm_2013_reseampled <- resample(chm_2013, chm_2004)
 
-#reseample 2013 to 2004 @2.5m
-chm_2013_reseampled<-resample(chm_2013, chm_2004)
-
-#calculate difference in CHM
+# Calcolo della differenza di CHM da 2013 a 2004
 difference<-chm_2013_reseampled-chm_2004
 
-#plot the difference
 ggplot() +
   geom_raster(difference, mapping =aes(x=x, y=y, fill=layer)) +
   scale_fill_viridis() +
   ggtitle("difference CHM San Genesio/Jenesien")
 
-
-#save the rasters
+# Salvataggio dei raster
 writeRaster(chm_2013_reseampled,"chm_2013_reseampled_San_genesio.tif")
 writeRaster(difference,"difference chm San_genesio.tif")
 
-
-
-
-
-## point cloud
+# Sezione dedicata al point cloud
 
 library(lidR)
 
-#load point cloud
-point_cloud<-readLAS("C:/Users/MiTorresani/OneDrive - Alma Mater Studiorum Universit� di Bologna/lezioni/UniBo/esercizi/dati/DownloadService/LIDAR-PointCloudCoverage-2013SolarTirol.laz")
+# load point cloud
+point_cloud<-readLAS(LIDAR-PointCloudCoverage-2013SolarTirol.laz")
 
 #plot r3 point cloud
 plot(point_cloud)
