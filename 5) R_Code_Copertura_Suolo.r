b22ac475ef5119e2228cdb00c34985af09273d13
MagriLuca <142036718+MagriLuca@users.noreply.github.com>
2023-08-26 13:44:24 +0200

    Update 5) R_Code_Copertura_Suolo.r

--- 5) R_Code_Copertura_Suolo.r
+++ 5) R_Code_Copertura_Suolo.r
@@ -7,9 +7,9 @@ library(patchwork)
 setwd("C:/lab/")
 
 # Numerazione delle bande
-# NIR 1
-# RED 2
-# GREEN 3
+# NIR = 1
+# RED = 2
+# GREEN = 3
 
 defor1 <- brick("defor1_.png")
 plotRGB(defor1, r=1, g=2, b=3, stretch="lin")
@@ -37,12 +37,13 @@ defor1class <- setValues(defor1[[1]], kcluster1$cluster)
 
 cl <- colorRampPalette(c('yellow','black','red'))(100)
 plot(defor1class, col=cl)
-# class 1: Foresta
-# class 2: Agricultura
+# Classe 1: Foresta
+# Classe 2: Agricultura
 
 # Funzione set.seed() consente di ottenere stessi risultati
 
 # Classificazione "Non supervisionata" per il secondo file
+
 # 1. Ottenimento tutti singoli valori
 singlenr2 <- getValues(defor2)
 singlenr2
@@ -78,7 +79,6 @@ percent_1992 <- c(89.83, 10.16)
 percent_2006 <- c(52.06, 47.93)
 
 percentages <- data.frame(cover, percent_1992, percent_2006)
-percentages
 
 # Plot di questi ultimi
 ggplot(percentages, aes(x=cover, y=percent_1992, color=cover)) + geom_bar(stat="identity", fill="white")
@@ -90,11 +90,11 @@ p2 <- ggplot(percentages, aes(x=cover, y=percent_2006, color=cover)) + geom_bar(
 p1+p2
 
 # Stessi limiti percentuali
-p1 <- ggplot(percentages, aes(x=cover, y=percent_1992, color=cover)) + geom_bar(stat="identity", fill="white") + 
+p1 <- ggplot(percentages, aes(x=cover, y=percent_1992, color=cover)) + geom_bar(stat="identity", fill="white") +
 ylim(c(0,100))
 
 p2 <- ggplot(percentages, aes(x=cover, y=percent_2006, color=cover)) + geom_bar(stat="identity",
-fill="white") + 
+fill="white") +
 ylim(c(0,100))
 
 p1+p2
