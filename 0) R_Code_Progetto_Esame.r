76f22f977b250b413d65227490ac87fb9fd273fc
MagriLuca <142036718+MagriLuca@users.noreply.github.com>
2023-08-30 14:16:35 +0200

    Update 0) R_Code_Progetto_Esame.r

--- 0) R_Code_Progetto_Esame.r
+++ 0) R_Code_Progetto_Esame.r
@@ -1,3 +1,18 @@
 # Questo è il codice R adoperato per il progetto d'esame
 
 library(raster)
+library(viridis)
+
+# Bande d'interesse in Landsat 5
+
+# Banda 1: Blu
+# Banda 2: Verde
+# Banda 3: Rosso
+# Banda 4: NIR
+
+# Bande d'interesse in Landsat 8
+
+# Banda 2: Blu
+# Banda 3: Verde
+# Banda 4: Rosso
+# Banda 5: NIR
