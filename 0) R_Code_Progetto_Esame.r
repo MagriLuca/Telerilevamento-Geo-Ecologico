ec29b2fa1d4b7b753d064c35cd668e53c8eaffb2
MagriLuca <142036718+MagriLuca@users.noreply.github.com>
2023-08-30 20:50:48 +0200

    Update 0) R_Code_Progetto_Esame.r

--- 0) R_Code_Progetto_Esame.r
+++ 0) R_Code_Progetto_Esame.r
@@ -1,7 +1,25 @@
 # Questo è il codice R adoperato per il progetto d'esame
 
+# Il progetto si focalizza sulla deforestazione in Brasile.
+# L'analisi verrà compiuta sulle seguenti tre grandi aree:
+
+# 1) Zona Ovest -----> Stato federale di Rondonia (Località Ariquemes)
+# 2) Zona Centrale --> Stato federale di Mato Grosso (Località Colniza)
+# 3) Zona Est -------> Stato federale di Parà (Località Marabà)
+
+# Ogni cella Landsat possiede una dimensione di 185 x 170 chilometri.
+# Nel lavoro sono state usate scansioni da Landsat 5 ed 8 sulle celle:
+
+# 1) Zona Ovest -----> Cella: Path 232 Row 067
+# 2) Zona Centrale --> Cella: Path 229 Row 067
+# 3) Zona Est -------> Cella: Path 223 Row 064
+
+# Temporalmente le analisi sono compiute nel mese di luglio (inverno);
+# In 5 diversi anni, ossia: 1987, 1996, 2005, 2014 e 2023.
+
 library(raster)
 library(rgdal)
+library(ggplot2)
 library(viridis)
 
 # Bande d'interesse in Landsat 5
@@ -99,4 +117,36 @@ ListaEst2023 <- list.files(pattern="Banda")
 ImportEst2023 <- lapply(ListaEst2023,raster)
 StackEst2023 <- stack(ImportEst2023)
 
-# Calcolo NDVI
+# Calcolo DVI e NDVI per Sezione Ovest e Differenza tra 1987 e 2023
+
+DVIStackOvest1987 = StackOvest1987[[4]] - StackOvest1987[[3]]
+NDVIStackOvest1987 = DVIStackOvest1987 / (StackOvest1987[[4]] + StackOvest1987[[3]])
+
+DVIStackOvest2023 = StackOvest2023[[4]] - StackOvest2023[[3]]
+NDVIStackOvest2023 = DVIStackOvest2023 / (StackOvest2023[[4]] + StackOvest2023[[3]])
+
+DifferenzaDVIOvest = DVIStackOvest1987 - DVIStackOvest2023
+Palette1 = colorRampPalette(c('blue','white','red'))(100)
+plot(DifferenzaDVIOvest, col = Palette1)
+
+# Calcolo DVI e NDVI per Sezione Centrale e Differenza tra 1987 e 2023
+
+DVIStackCentrale1987 = StackCentrale1987[[4]] - StackCentrale1987[[3]]
+NDVIStackCentrale1987 = DVIStackCentrale1987 / (StackCentrale1987[[4]] + StackCentrale1987[[3]])
+
+DVIStackCentrale2023 = StackCentrale2023[[4]] - StackCentrale2023[[3]]
+NDVIStackCentrale2023 = DVIStackCentrale2023 / (StackCentrale2023[[4]] + StackCentrale2023[[3]])
+
+DifferenzaDVICentrale = DVIStackCentrale1987 - DVIStackCentrale2023
+plot(DifferenzaDVICentrale, col = Palette1)
+
+# Calcolo DVI e NDVI per Sezione Est e Differenza tra 1987 e 2023
+
+DVIStackEst1987 = StackEst1987[[4]] - StackEst1987[[3]]
+NDVIStackEst1987 = DVIStackEst1987 / (StackEst1987[[4]] + StackEst1987[[3]])
+
+DVIStackEst2023 = StackEst2023[[4]] - StackEst2023[[3]]
+NDVIStackEst2023 = DVIStackEst2023 / (StackEst2023[[4]] + StackEst2023[[3]])
+
+DifferenzaDVIEst = DVIStackEst1987 - DVIStackEst2023
+plot(DifferenzaDVIEst, col = Palette1)
