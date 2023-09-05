fea8b2eefa805b4721a905bd1d1da20a7e7251f3
MagriLuca <142036718+MagriLuca@users.noreply.github.com>
2023-09-05 14:07:43 +0200

    Update R_Code.r

--- R_Code.r
+++ R_Code.r
@@ -1,4 +1,4 @@
-DifferenzaDVIOvest = DVIW1987 - DVIW2023
-tiff("DifferenzaDVIOvest.tiff")
-plot(DifferenzaDVIOvest, col=Palette1)
-dev.off()
+W1987 <- brick("StackW1987Cut.tiff")
+SingleValuesW1987 <- getValues(W1987)
+kclusterW1987 <- kmeans(SingleValuesW1987, centers = 2)
+W1987Class <- setValues(W1987[[1]], kclusterW1987$cluster)
