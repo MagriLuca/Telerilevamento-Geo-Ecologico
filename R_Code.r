c69cdc06ed4b6557afddf3e4f646c28119f68c87
MagriLuca <142036718+MagriLuca@users.noreply.github.com>
2023-09-05 12:43:04 +0200

    Update R_Code.r

--- R_Code.r
+++ R_Code.r
@@ -1,4 +1,4 @@
-setwd("C:/Telerilevamento/Brasile_Ovest/232_067")
-ListaOvest1987 <- list.files(pattern="Banda")
-ImportOvest1987 <- lapply(ListaOvest1987,raster)
-StackOvest1987 <- stack(ImportOvest1987)
+setwd("C:/Telerilevamento/Elaborati")
+tiff("StackOvest1987.tiff")
+plotRGB(StackOvest1987, 4, 2, 1, stretch="Lin")
+dev.off()
