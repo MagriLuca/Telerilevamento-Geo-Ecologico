933be811b4a9e567544e32bfaf5bca7656a004d1
MagriLuca <142036718+MagriLuca@users.noreply.github.com>
2023-09-05 13:33:32 +0200

    Update R_Code.r

--- R_Code.r
+++ R_Code.r
@@ -1,4 +1,4 @@
-DVIOvest1987 = StackOvest1987[[4]] - StackOvest1987[[3]]
-NDVIOvest1987 = DVIOvest1987 / (StackOvest1987[[4]] + 
-                                + StackOvest1987[[3]])
-plot(NDVIOvest1987, col=Palette1)
+DVIW1987 = StackOvest1987[[4]] - StackOvest1987[[3]]
+NDVIW1987 = DVIW1987 / (StackOvest1987[[4]] + 
+                            + StackOvest1987[[3]])
+plot(NDVIW1987, col=Palette1)
